90934c7bfd03dd3d5365dc383cbf05da977cc15f
Santiago Niveyro <sniveyro@algeiba.com>
2023-07-03 15:03:21 -0300

    Agregar archivos de proyecto.

---
+++ FormRecognizer-Lectura/Program.cs
@@ -0,0 +1,58 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+
+//use your `key` and `endpoint` environment variables to create your `AzureKeyCredential` and `DocumentAnalysisClient` instances
+string key = "3cefc52d84204f3dbc295d3a3cc9ed7f";
+string endpoint = "https://demorecognizer15.cognitiveservices.azure.com/";
+AzureKeyCredential credential = new AzureKeyCredential(key);
+DocumentAnalysisClient client = new DocumentAnalysisClient(new Uri(endpoint), credential);
+
+//sample document
+Uri fileUri = new Uri("https://recognizer.blob.core.windows.net/demo/sample-brochure.png?sp=r&st=2023-07-01T03:28:59Z&se=2023-07-19T11:28:59Z&spr=https&sv=2022-11-02&sr=b&sig=p%2FY1MoW4PE2M54ZkJhBgGvpwaaXIzGr%2BD1Ou%2BLPRXfY%3D");
+
+AnalyzeDocumentOperation operation = await client.AnalyzeDocumentFromUriAsync(WaitUntil.Completed, "prebuilt-read", fileUri);
+AnalyzeResult result = operation.Value;
+
+foreach (DocumentPage page in result.Pages)
+{
+    Console.WriteLine($"Document Page {page.PageNumber} has {page.Lines.Count} line(s), {page.Words.Count} word(s),");
+    Console.WriteLine($"and {page.SelectionMarks.Count} selection mark(s).");
+
+    for (int i = 0; i < page.Lines.Count; i++)
+    {
+        DocumentLine line = page.Lines[i];
+        Console.WriteLine($"  Line {i} has content: '{line.Content}'.");
+
+        Console.WriteLine($"    Its bounding polygon (points ordered clockwise):");
+
+        for (int j = 0; j < line.BoundingPolygon.Count; j++)
+        {
+            Console.WriteLine($"      Point {j} => X: {line.BoundingPolygon[j].X}, Y: {line.BoundingPolygon[j].Y}");
+        }
+    }
+}
+
+foreach (DocumentStyle style in result.Styles)
+{
+    // Check the style and style confidence to see if text is handwritten.
+    // Note that value '0.8' is used as an example.
+
+    bool isHandwritten = style.IsHandwritten.HasValue && style.IsHandwritten == true;
+
+    if (isHandwritten && style.Confidence > 0.8)
+    {
+        Console.WriteLine($"Handwritten content found:");
+
+        foreach (DocumentSpan span in style.Spans)
+        {
+            Console.WriteLine($"  Content: {result.Content.Substring(span.Index, span.Length)}");
+        }
+    }
+}
+
+Console.WriteLine("Detected languages:");
+
+foreach (DocumentLanguage language in result.Languages)
+{
+    Console.WriteLine($"  Found language with locale'{language.Locale}' with confidence {language.Confidence}.");
+}
